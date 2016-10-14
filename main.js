804da5c3e3a7326b16acc367b9943901d6913e9f
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-14 16:28:47 -0400

    massive progress with main box

---
+++ main.js
@@ -0,0 +1,30 @@
+var results = data.results;
+
+
+//console.log(data);
+
+function renderItem (item) {
+  return `
+    
+   <div class="box1">${item.title}</div>
+  `;
+}
+
+function whiskeyGrid () {
+	// var image = results[0].Images[13];
+	// var title = results[0].title;
+	// var price = results[0].price;
+	// var user  = results[0].who_made;
+
+ for (var i = 0; i < results.length; i++){
+ 	//console.log(results[i]);
+
+ 	//`<div>${results[i].title}</div>`
+// 	$("#grid").append(`<p> ${results[i].title}</p>`);
+    var itemHtml = renderItem(results[i]);
+ 	$("#grid").append(itemHtml);
+ };
+
+}
+
+whiskeyGrid();
