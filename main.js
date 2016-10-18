aa34fb9a99dfb02663c3ed93c165c6646fe2463b
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-18 16:41:36 -0400

    mad progress

--- main.js
+++ main.js
@@ -7,25 +7,25 @@ function renderItem (item) {
   return `
     
    <div class="box1">
-   <img src="${item.Images[0].url_170x135}">
-   <p>${item.title}</p>
-   <p>${item.price}</p>
+   <a href="${item.url}">
+   <img src="${item.Images[0].url_170x135}"">
+   <div class="underBox">
+   <p class="titleoverlap">${item.title}</p>
 
+   <div class="bottomSmall">
+<p class="shopName">${item.Shop.shop_name}</p>
+   <p class="itemPrice">$${item.price}</p>
+   </div>
+   </div>
+   </a>
    </div>
   `;
 }
 
 function whiskeyGrid () {
-	// var image = results[0].Images[13];
-	// var title = results[0].title;
-	// var price = results[0].price;
-	// var user  = results[0].who_made;
 
- for (var i = 0; i < results.length; i++){
- 	//console.log(results[i]);
+ for (var i = 0; i < results.length -1; i++){
 
- 	//`<div>${results[i].title}</div>`
-// 	$("#grid").append(`<p> ${results[i].title}</p>`);
     var itemHtml = renderItem(results[i]);
  	$("#grid").append(itemHtml);
  };
